a6b0e441d924107a60ab49af2dec08e9d78d5603
seungaggu <babycare_seung@naver.com>
2025-06-17 09:28:34 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,11 @@
+a,b,c=map(int,input().split())
+
+if a==b==c:
+    m=10000+(a*1000)
+elif a==b or a==c:
+    m= 1000+(a*100)
+elif b==c:
+    m= 1000+(b*100)
+else:
+    m= max(a,b,c)*100
+print(m)
